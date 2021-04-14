8105b6172190dc4b832611f5a46dc5d071d40f16
Beat Murmann <bm@beato.ch>
2021-04-14 20:15:18 +0200

    package.swift changed target file ext

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
             // Targets can depend on other targets in this package, and on products in packages this package depends on.
             .binaryTarget(
                 name: "SwiftyChrono",
-                path:"/Users/bm/Library/Developer/Xcode/DerivedData/SwiftyChrono-enrhvvbvxjfnmeemfswurkibeigz/Build/Products/Debug-iphoneos/SwiftyChrono.framework"
+                path:"/Users/bm/Library/Developer/Xcode/DerivedData/SwiftyChrono-enrhvvbvxjfnmeemfswurkibeigz/Build/Products/Debug-iphoneos/SwiftyChrono.xcframework"
             )
         ]
 )
