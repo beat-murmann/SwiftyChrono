b2c6447935497fa0906a4b612dbc11cce2e2ac44
Beat Murmann <bm@beato.ch>
2021-04-14 20:12:00 +0200

    package.swift removed double target

--- Package.swift
+++ Package.swift
@@ -18,9 +18,6 @@ let package = Package(
         targets: [
             // Targets are the basic building blocks of a package. A target can define a module or a test suite.
             // Targets can depend on other targets in this package, and on products in packages this package depends on.
-            .target(
-                name: "SwiftyChrono"
-            ),
             .binaryTarget(
                 name: "SwiftyChrono",
                 path:"/Users/bm/Library/Developer/Xcode/DerivedData/SwiftyChrono-enrhvvbvxjfnmeemfswurkibeigz/Build/Products/Debug-iphoneos/SwiftyChrono.framework"
